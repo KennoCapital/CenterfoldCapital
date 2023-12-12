7c96e9b5298be132f970918651badd01d192922c
KennoCapital <kenno.nicklas@gmail.com>
2023-12-12 13:31:16 +0100

    ts ADD DiffReg EuPayerSwpt

--- application/experiments/trolle_schwartz/diff_ML_plots/ts_AAD_DiffReg_plot_delta_EuPayerSwpt.py
+++ application/experiments/trolle_schwartz/diff_ML_plots/ts_AAD_DiffReg_plot_delta_EuPayerSwpt.py
@@ -1,13 +1,16 @@
+import os
+import pickle
 import matplotlib.pyplot as plt
-
 import torch
-from torch.func import jacfwd
+from torch.func import jacfwd, vmap
 from application.engine.trolleSchwartz import trolleSchwartz
 from application.engine.mcBase import mcSim, RNG
 from application.engine.products import EuropeanPayerSwaption
 from application.engine.differential_Regression import DifferentialPolynomialRegressor
 from application.engine.standard_scalar import DifferentialStandardScaler
+from application.utils.path_config import get_data_path
 from functools import partial
+from tqdm import tqdm
 
 torch.set_printoptions(4)
 torch.set_default_dtype(torch.float64)
@@ -85,22 +88,34 @@ def calc_swap(
 
 
 if __name__ == '__main__':
+    filename_path = get_data_path('ts_EuPayerSwpt_testset.pkl')
+
     seed = 1234
     N_train = 512
-    N_test = 16 ** 2
+    N_test = 32  # Total test cases are N_test squared
     steps_per_year = 100
     use_av = True
 
+    # Monte Carlo specification
+    rng = RNG(seed=seed, use_av=use_av)
+
+    # Setup Differential Regressor, and Scalar
+    deg = 5
+    alpha = 1.0
+    include_interactions = True
+    diff_reg = DifferentialPolynomialRegressor(deg=deg, alpha=alpha, use_SVD=True, bias=True, include_interactions=include_interactions)
+    scalar = DifferentialStandardScaler()
+
     # Model specification
     simDim = 1
-    xt = torch.tensor([0.0])
-    vt = torch.tensor([0.0194])
-    phi1t = torch.tensor([0.0])
-    phi2t = torch.tensor([0.075])
-    phi3t = torch.tensor([0.040])
-    phi4t = torch.tensor([0.200])
-    phi5t = torch.tensor([0.060])
-    phi6t = torch.tensor([0.165])
+    xt = (-0.5 - 0.5) * torch.rand((N_train, ), generator=rng.gen) + 0.5
+    vt = (1e-6 - 0.5) * torch.rand((N_train, ), generator=rng.gen) + 0.5
+    phi1t = torch.tensor([0.0]) * torch.ones(N_train)
+    phi2t = torch.tensor([0.075]) * torch.ones(N_train)
+    phi3t = torch.tensor([0.040]) * torch.ones(N_train)
+    phi4t = torch.tensor([0.200]) * torch.ones(N_train)
+    phi5t = torch.tensor([0.060]) * torch.ones(N_train)
+    phi6t = torch.tensor([0.165]) * torch.ones(N_train)
 
     kappa = torch.tensor(0.0553) 
     theta = torch.tensor(0.7542) * kappa / torch.tensor(2.1476)
@@ -113,14 +128,7 @@ if __name__ == '__main__':
 
     varphi = torch.tensor(0.0832)
 
-    # Random grid in U[a, b] x U[c, d]
-
-
-    #v0 = (1E-6 - 0.5) * torch.rand(N_train) + 0.5
-    #varphi = (-0.02 - 0.15) * torch.rand(N_train) + 0.15
-    #grid = torch.hstack((v0, varphi))
-
-    const = kappa, theta, sigma, rho, varphi, gamma, alpha0, alpha1
+    const = kappa, theta, sigma, rho, gamma, alpha0, alpha1, varphi
 
     mdl = trolleSchwartz(
         xt=xt, vt=vt, phi1t=phi1t, phi2t=phi2t, phi3t=phi3t, phi4t=phi4t, phi5t=phi5t, phi6t=phi6t,
@@ -146,44 +154,73 @@ if __name__ == '__main__':
         notional=notional
     )
 
-    # Monte Carlo specification
-    rng = RNG(seed=seed, use_av=use_av)
     dTL = torch.linspace(0.0, float(exerciseDate), int(exerciseDate * steps_per_year) + 1)
 
     # Auxiliary functions
-    x, v, phi1, phi2, phi3, phi4, phi5, phi6 = mdl.x0
-    f_V = partial(calc_payoff_EuPayerSwpt, prd=prd, rng=rng, N=N_train, dTL=dTL, simDim=1)
-    f_swap = partial(calc_swap, fixings=prd.swapFixingDates, delta=prd.delta, K=prd.strike, N=prd.notional, simDim=1)
+    f_V = partial(
+        calc_payoff_EuPayerSwpt,
+        kappa=kappa, theta=theta, rho=rho, sigma=sigma, alpha0=alpha0, alpha1=alpha1, gamma=gamma, varphi=varphi,
+        prd=prd, rng=rng, N=N_train, dTL=dTL, simDim=1
+    )
+    f_swap = partial(
+        calc_swap,
+        # x, v, phi1, phi2, phi3, phi4, phi5, phi6,
+        kappa=kappa, theta=theta, rho=rho, sigma=sigma, alpha0=alpha0, alpha1=alpha1, gamma=gamma,
+        varphi=varphi,
+        fixings=prd.swapFixingDates, delta=prd.delta, K=prd.strike, N=prd.notional, simDim=1
+    )
 
     # Compute Jacobians and values
-    ones = torch.ones(N_train)
+    ones = torch.ones(N_train, 1)
     state = mdl.x0
-    Jswap, swap = jacfwd(f_swap, argnums=(0, 1, 2, 3, 4, 5, 6), has_aux=True)(*state, *const)
-    Jpayoff, payoff = jacfwd(f_V, argnums=(0, 1, 2, 3, 4, 5, 6), has_aux=True, randomness='same')(*state, *const)
+    Jswap, swap = jacfwd(f_swap, argnums=(0, 1, 2, 3, 4, 5, 6, 7), has_aux=True)(*state)
+    Jpayoff, payoff = jacfwd(f_V, argnums=(0, 1, 2, 3, 4, 5, 6, 7), has_aux=True, randomness='same')(*state)
 
-    dU_dParam = torch.hstack([J @ ones for J in Jswap])
-    dPayoff_dParam = torch.hstack([J @ ones for J in Jpayoff])
+    dU_dParam = torch.hstack([J.squeeze() @ ones for J in Jswap])
+    dPayoff_dParam = torch.hstack([J.squeeze() @ ones for J in Jpayoff])
 
     # Trick using the chain-rule as stated in "Fixed Income Derivatives" p. 28
     dVdu = torch.full((N_train, 1), torch.nan)
     for i in range(N_train):
         dVdu[i, 0] = torch.pinverse(dU_dParam[i, :].reshape(1, -1).T) @ dPayoff_dParam[i, :].reshape(-1, 1)
 
-    # Setup Differential Regressor, and Scalar
-    deg = 5
-    alpha = 1.0
-    include_interactions = True
-    diff_reg = DifferentialPolynomialRegressor(deg=deg, alpha=alpha, use_SVD=True, bias=True, include_interactions=include_interactions)
-    scalar = DifferentialStandardScaler()
+    # Make test dataset for comparison
+    xt_test = torch.linspace(xt.min(), xt.max(), N_test)
+    vt_test = torch.linspace(vt.min(), vt.max(), N_test)
 
-    x_train = torch.hstack([swap, v0.reshape(-1, 1)])
+    if os.path.isfile(filename_path):
+        with open(filename_path, 'rb') as file:
+            x_test, y_test, z_test, swap_test = pickle.load(file)
+    else:
+        y_test = torch.full((N_test, N_test), torch.nan)
+        swap_test = torch.full((N_test, N_test), torch.nan)
+        for i, xt_ in tqdm(enumerate(xt_test), total=N_test, desc='Calculating test dataset with MC'):
+            for j, vt_ in enumerate(vt_test):
+                cMdl = trolleSchwartz(
+                    xt=xt_, vt=vt_,
+                    phi1t=phi1t[0], phi2t=phi2t[0], phi3t=phi3t[0], phi4t=phi4t[0], phi5t=phi5t[0], phi6t=phi6t[0],
+                    kappa=kappa, theta=theta, sigma=sigma, rho=rho,
+                    gamma=gamma, alpha0=alpha0, alpha1=alpha1, varphi=varphi,
+                    simDim=simDim
+                )
+                swap_test[i, j] = cMdl.calc_swap(cMdl.x0, prd.swapFixingDates, prd.delta, prd.strike, prd.notional)
+                y_test[i, j] = torch.mean(mcSim(prd, cMdl, rng, 50000, dTL))
+
+        x_test = torch.cartesian_prod(swap_test[:, 0], vt_test)
+        delta_test = y_test.diff(dim=0) / swap_test.diff(dim=0)
+        vega_test = y_test.diff(dim=1) / vt_test.diff()
+        z_test = [delta_test, vega_test]
+
+        with open(filename_path, 'wb') as file:
+            test_data = tuple((x_test, y_test, z_test, swap_test))
+            pickle.dump(test_data, file, pickle.HIGHEST_PROTOCOL)
+
+    # Make training dataset
+    x_train = torch.hstack([swap, vt.reshape(-1, 1)])
     y_train = payoff
     z_train = torch.hstack([dVdu, dPayoff_dParam[:, 1].reshape(-1, 1)])
 
-    swap_test = torch.linspace(swap.min(), swap.max(), 32)
-    v0_test = torch.linspace(v0.min(), v0.max(), 32)
-    x_test = torch.cartesian_prod(swap_test, v0_test)
-
+    # Differential regression
     x_train_scaled, y_train_scaled, z_train_scaled = scalar.fit_transform(x_train, y_train, z_train)
 
     diff_reg.fit(x_train_scaled, y_train_scaled, z_train_scaled)
@@ -193,21 +230,23 @@ if __name__ == '__main__':
 
     _, y_pred, z_pred = scalar.predict(None, y_pred_scaled, z_pred_scaled)
 
-    " Plot Results "
+    ''' Plot Results '''
+
     # Data for plotting a surface
-    x_ = x_test[:, 0].reshape(32, 32)
-    y_ = x_test[:, 1].reshape(32, 32)
-    z_price = y_pred.reshape(32, 32)
-    z_delta = z_pred[:, 0].reshape(32, 32)
-    z_vega = z_pred[:, 1].reshape(32, 32)
-    zero = torch.zeros(32, 32)
+    x_ = x_test[:, 0].reshape(N_test, N_test)
+    y_ = x_test[:, 1].reshape(N_test, N_test)
+    z_price = y_pred.reshape(N_test, N_test)
+    z_delta = z_pred[:, 0].reshape(N_test, N_test)
+    z_vega = z_pred[:, 1].reshape(N_test, N_test)
+    zero = torch.zeros(N_test, N_test)
+
+    vt_test = torch.hstack([vt for i in range(N_test)])
 
     # Price
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, sharex='all', sharey='all')
-    surf_train = ax.scatter(swap, v0, payoff, c='gray', alpha=1.0)
-    # surf_pred = ax.scatter(x_, y_, z_, c=y_pred, cmap=plt.cm.magma)
+    surf_train = ax.scatter(swap, vt, payoff, c='gray', alpha=1.0)
+    surf_test = ax.scatter(x_test[:, 0], x_test[:, 1], y_test.flatten(), c='black')
     surf_pred = ax.plot_surface(x_, y_, z_price, cmap=plt.cm.magma)
-    # surf_zero = ax.plot_surface(x_, y_, zero, color='blue', alpha=0.5)
 
     ax.set_xlabel('Swap(0)')
     ax.set_ylabel('v(0)')
@@ -218,28 +257,32 @@ if __name__ == '__main__':
 
     # Delta
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, sharex='all', sharey='all')
-    surf_train = ax.scatter(swap, v0, z_train[:, 0], c='gray', alpha=1.0)
+    surf_train = ax.scatter(swap, vt, z_train[:, 0], c='gray', alpha=1.0)
+    surf_test = ax.scatter(x_test[:, 0].reshape(N_test, N_test)[1:].flatten(),
+                           x_test[:, 1].reshape(N_test, N_test)[1:].flatten(),
+                           z_test[0].flatten(), c='black')
     surf_pred = ax.plot_surface(x_, y_, z_delta, cmap=plt.cm.magma)
 
     ax.set_xlabel('Swap(0)')
     ax.set_ylabel('v(0)')
     ax.set_zlabel('Delta')
 
-    # Add a color bar which maps values to colors.
     fig.colorbar(surf_pred, shrink=0.5, aspect=5)
     plt.show()
 
     # Vega
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, sharex='all', sharey='all')
-    surf_train = ax.scatter(swap, v0, z_train[:, 1], c='gray', alpha=1.0)
-    surf_pred = ax.plot_surface(x_, y_, z_vega, cmap=plt.cm.magma)
+    #surf_train = ax.scatter(swap, vt, z_train[:, 1] / 10000, c='gray', alpha=1.0)
+    surf_test = ax.scatter(x_test[:, 0].reshape(N_test, N_test)[1:].flatten(),
+                           x_test[:, 1].reshape(N_test, N_test)[1:].flatten(),
+                           z_test[1].flatten() / 10000, c='black')
+    surf_pred = ax.plot_surface(x_, y_, z_vega / 10000, cmap=plt.cm.magma)
 
     ax.set_xlabel('Swap(0)')
     ax.set_ylabel('v(0)')
-    ax.set_zlabel('Payoff')
+    ax.set_zlabel('Vega (per bp)')
+    ax.set_zlim(-50, 150)
 
-    # Add a color bar which maps values to colors.
     fig.colorbar(surf_pred, shrink=0.5, aspect=5)
     plt.show()
-
 
